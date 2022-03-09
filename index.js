deea4d70ddb7281aafdd0e74b47c188b0210403d
Tom Nicholson <tfwnicholson+1@gmail.com>
2022-03-09 21:18:42 +0000

    Buttons play the right sounds

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const handleClick = _ => {
-    alert("I got clicked");
+const soundMap = {
+    "w": "crash",
+    "a": "kick-bass",
+    "s": "snare",
+    "d": "tom-1",
+    "j": "tom-2",
+    "k": "tom-3",
+    "l": "tom-4"
+};
+const handleClick = e => {
+    const buttonLetter = e.target.className.substr(0, 1);
+    let audio = new Audio(`sounds/${soundMap[buttonLetter]}.mp3`);
+    audio.play();
 }
 
 Array.from(
